7b6541e6e02b1e166653a32ad827ce73707f0239
Roscoe A. Bartlett <rabartl@sandia.gov>
2019-02-19 05:55:32 -0700

    Switch from git@github.com to https://github.com (tribitspub/tribits#278)

--- cmake/ExtraRepositoriesList.cmake
+++ cmake/ExtraRepositoriesList.cmake
@@ -1,4 +1,4 @@
-SET_DEFAULT_AND_FROM_ENV(GIT_URL_REPO_BASE  git@github.com:tribits/)
+SET_DEFAULT_AND_FROM_ENV(GIT_URL_REPO_BASE  https://github.com/tribits/)
 
 TRIBITS_PROJECT_DEFINE_EXTRA_REPOSITORIES(
   TribitsExampleProject   ""  GIT  ${GIT_URL_REPO_BASE}TribitsExampleProject.git
